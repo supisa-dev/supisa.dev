1830f970264e786d23c219781682a894b9513bcf
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-03-21 23:06:30 +0900

    build: tailwind config에 폰트 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        's-core-dream-100': ['S Core Dream 100', 'sans-serif'],
+        's-core-dream-200': ['S Core Dream 200', 'sans-serif'],
+        's-core-dream-300': ['S Core Dream 300', 'sans-serif'],
+        's-core-dream-400': ['S Core Dream 400', 'sans-serif'],
+        's-core-dream-500': ['S Core Dream 500', 'sans-serif'],
+        's-core-dream-600': ['S Core Dream 600', 'sans-serif'],
+        's-core-dream-700': ['S Core Dream 700', 'sans-serif'],
+        's-core-dream-800': ['S Core Dream 800', 'sans-serif'],
+        's-core-dream-900': ['S Core Dream 900', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
