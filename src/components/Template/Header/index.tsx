29a93faa35e63558f31cf4478f41c7f4c8c0bfb1
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-03-23 23:47:21 +0900

    feat: header 컴포넌트 작업

--- src/components/Template/Header/index.tsx
+++ src/components/Template/Header/index.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <></>;
+  return (
+    <header className="fixed top-0 left-0 flex flex-row justify-center w-full h-[80px] bg-[#2B2B2B]">
+      <div className="w-[90%] h-full">
+        <a className="">
+          <img></img>
+        </a>
+      </div>
+    </header>
+  );
 }
