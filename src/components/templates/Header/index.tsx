dc2010080d47fd96b645df435a9c14720b9cf78a
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-03-30 23:36:25 +0900

    feat: dark mode 커스텀 훅 & store 생성

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -5,7 +5,7 @@ import ThemeToggleSwitch from '@/components/block/ThemeToggleSwitch';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full h-[80px] bg-[#2B2B2B] border-b-[1px] border-solid border-[#cecece]">
+    <header className="fixed top-0 left-0 w-full h-[80px] bg-[#cecece] dark:bg-[#2B2B2B] border-b-[1px] border-solid b border-[#cecece]">
       <div className="absolute top-0 left-0 w-full h-full flex flex-row justify-center ">
         <div className="relative w-[90%] h-full">
           <a className="absolute top-1/2 left-0 -translate-y-1/2 inline-block w-auto h-auto cursor-pointer">
