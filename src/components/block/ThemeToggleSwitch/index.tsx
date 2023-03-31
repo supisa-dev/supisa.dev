adef7f9a09d10dc9cc6c97ff634c03062fa2e66d
SUNG PILLSANG <pillsang@i-ve.kr>
2023-03-31 13:26:08 +0900

    feat: theme 커스텀 훅 생성

--- src/components/block/ThemeToggleSwitch/index.tsx
+++ src/components/block/ThemeToggleSwitch/index.tsx
@@ -1,38 +1,23 @@
-import {useState} from 'react';
 import {BsFillSunFill, BsFillMoonStarsFill} from 'react-icons/bs';
+import useTheme from '@/components/hooks/useTheme';
 
 export default function ThemeToggleSwitch() {
-  const [localTheme, setLocalTheme] = useState(false);
+  const {theme, changeTheme} = useTheme();
+
   return (
     <div
       onClick={() => {
-        setLocalTheme((prev) => !prev);
+        changeTheme();
       }}
-      className={`absolute top-1/2 right-16 -translate-y-1/2 w-[70px] h-[30px] cursor-pointer ${
-        localTheme ? 'bg-[#525355]' : 'bg-[#FEDD40]'
-      } rounded-3xl transition-all duration-200 ease-linear`}
+      className="absolute top-1/2 right-16 -translate-y-1/2 w-[70px] h-[30px] cursor-pointer bg-[#FEDD40] dark:bg-[#525355] rounded-3xl transition-all duration-200 ease-linear"
     >
-      <p
-        className={`absolute top-1/2 ${
-          localTheme ? 'right-[30px]' : 'right-[15px]'
-        } -translate-y-[40%] inline-block w-auto h-auto`}
-      >
-        {localTheme ? (
-          <span className="block font-SCoreDream700 text-[10px] leading-[14px] text-white">
-            NIGHT
-          </span>
-        ) : (
-          <span className="block font-SCoreDream700 text-[10px] leading-[14px] text-white">
-            DAY
-          </span>
-        )}
+      <p className="absolute top-1/2 right-[15px] dark:right-[30px] -translate-y-[40%] inline-block w-auto h-auto">
+        <span className="block font-SCoreDream700 text-[10px] leading-[14px] text-white">
+          {theme ? 'NIGHT' : 'DAY'}
+        </span>
       </p>
-      <div
-        className={`absolute top-1/2 ${
-          localTheme ? 'left-[46px]' : 'left-1'
-        } -translate-y-1/2 w-[20px] h-[20px] rounded-full bg-white transition-all duration-200 ease-linear`}
-      >
-        {localTheme ? (
+      <div className="absolute top-1/2 left-1 dark:left-[46px] -translate-y-1/2 w-[20px] h-[20px] rounded-full bg-white transition-all duration-200 ease-linear">
+        {theme ? (
           <BsFillMoonStarsFill
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             size="13"
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,17 @@
 import {Html, Head, Main, NextScript} from 'next/document';
 
 export default function Document() {
+  const checkTheme = () => {
+    return {
+      __html: `
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+    `,
+    };
+  };
   return (
     <Html lang="ko">
       <Head>
@@ -14,6 +25,7 @@ export default function Document() {
         />
       </Head>
       <body>
+        <script dangerouslySetInnerHTML={checkTheme()} />
         <Main />
         <NextScript />
       </body>
