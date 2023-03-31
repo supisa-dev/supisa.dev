1146ad9517af481a40b3829871dcdcc36418f5f0
SUNG PILLSANG <pillsang@i-ve.kr>
2023-03-31 11:36:26 +0900

    feat: zustand persist 이슈로 인해 로직 제거

--- src/components/block/ThemeToggleSwitch/index.tsx
+++ src/components/block/ThemeToggleSwitch/index.tsx
@@ -1,24 +1,23 @@
-// Package
 import {useState} from 'react';
 import {BsFillSunFill, BsFillMoonStarsFill} from 'react-icons/bs';
-// Store
-import themeStore from '@/store/themeStore';
 
 export default function ThemeToggleSwitch() {
-  const {theme, setTheme, getTheme} = themeStore();
+  const [localTheme, setLocalTheme] = useState(false);
   return (
     <div
-      onClick={() => setTheme('dark')}
+      onClick={() => {
+        setLocalTheme((prev) => !prev);
+      }}
       className={`absolute top-1/2 right-16 -translate-y-1/2 w-[70px] h-[30px] cursor-pointer ${
-        theme === 'light' ? 'bg-[#525355]' : 'bg-[#FEDD40]'
+        localTheme ? 'bg-[#525355]' : 'bg-[#FEDD40]'
       } rounded-3xl transition-all duration-200 ease-linear`}
     >
       <p
         className={`absolute top-1/2 ${
-          theme === 'light' ? 'right-[30px]' : 'right-[15px]'
+          localTheme ? 'right-[30px]' : 'right-[15px]'
         } -translate-y-[40%] inline-block w-auto h-auto`}
       >
-        {theme === 'light' ? (
+        {localTheme ? (
           <span className="block font-SCoreDream700 text-[10px] leading-[14px] text-white">
             NIGHT
           </span>
@@ -30,10 +29,10 @@ export default function ThemeToggleSwitch() {
       </p>
       <div
         className={`absolute top-1/2 ${
-          theme === 'light' ? 'left-[46px]' : 'left-1'
+          localTheme ? 'left-[46px]' : 'left-1'
         } -translate-y-1/2 w-[20px] h-[20px] rounded-full bg-white transition-all duration-200 ease-linear`}
       >
-        {theme === 'light' ? (
+        {localTheme ? (
           <BsFillMoonStarsFill
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             size="13"
