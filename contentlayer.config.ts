07ecfe4cbe0b6f5938f73b9d6806eb5f99986c4c
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-04-05 23:55:05 +0900

    refactor: es에서 commonjs로 변경

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,4 @@
-import {defineDocumentType, makeSource} from 'contentlayer/source-files';
+const {defineDocumentType, makeSource} = require('contentlayer/source-files');
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
