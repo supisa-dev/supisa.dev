394536c4b567bd20243bc6bc3ba51e55699ab87a
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-04-10 23:54:06 +0900

    feat: html 마크업 수정

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import ThemeToggleSwitch from '@/components/block/ThemeToggleSwitch';
 
@@ -6,9 +8,18 @@ export default function Header() {
     <header className="fixed top-0 left-0 w-full h-[80px] bg-[#cecece] dark:bg-[#2B2B2B] border-b-[1px] border-solid b border-[#cecece]">
       <div className="absolute top-0 left-0 w-full h-full flex flex-row justify-center ">
         <div className="relative w-[90%] h-full">
-          <a className="absolute top-1/2 left-0 -translate-y-1/2 inline-block w-auto h-auto cursor-pointer">
-            <img src="images/logo.webp" width="45" height="45" alt="supisa website logo image" />
-          </a>
+          <Link
+            href="/"
+            className="absolute top-1/2 left-0 -translate-y-1/2 inline-block w-auto h-auto cursor-pointer"
+          >
+            <Image
+              className="inline-block w-[2.8125rem] h-[2.8125rem]"
+              width="45"
+              height="45"
+              src="/images/logo.webp"
+              alt="supisa.dev website logo"
+            />
+          </Link>
           <ThemeToggleSwitch />
           <GiHamburgerMenu
             className="absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer"
