288d6909f19e7be5405329cb8cbe1c9f4e87b672
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-04-24 00:01:23 +0900

    style: pc 버전 헤더 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -59,7 +59,7 @@ export default function App({Component, pageProps}: AppProps) {
       ])} w-full h-screen absolute top-0 left-0 overflow-hidden flex flex-col items-center`}
     >
       <Layout>
-        <main className="w-full h-[calc(100%-80px)] absolute top-[80px] left-0 overflow-hidden">
+        <main className="w-full max-w-[1023px] h-[calc(100%-80px)] absolute top-[80px] left-0 overflow-hidden">
           <Component {...pageProps} />
         </main>
       </Layout>
