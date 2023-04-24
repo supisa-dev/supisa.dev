0aa3d946167bf790eea02bdaa21b74e6965687ef
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-04-24 11:13:19 +0900

    feat: aside 배경 흑백 적용

--- src/components/templates/Aside/index.tsx
+++ src/components/templates/Aside/index.tsx
@@ -9,25 +9,34 @@ interface AsideProps {
 
 export default function Aside({isDarkThemeActive, isAsideActive, setIsAsideActive}: AsideProps) {
   return (
-    <aside
-      className={`fixed top-0 right-0 w-[300px] h-screen z-10 bg-white dark:bg-[#1C2731] transition-transform duration-200 ease-in-out ${
-        isAsideActive ? '' : 'translate-x-full'
-      }`}
-    >
-      <section className="w-full h-full flex flex-col items-center justify-start">
-        <div
-          role="presentation"
-          className="w-full h-[80px] px-[24px] flex flex-row items-center justify-end"
-          onClick={() => setIsAsideActive(false)}
-        >
-          <BiArrowFromLeft
-            className="cursor-pointer"
-            onClick={() => setIsAsideActive(!isAsideActive)}
-            size="40"
-            color={isDarkThemeActive ? '#cecece' : '#1C2731'}
-          />
-        </div>
-      </section>
-    </aside>
+    <>
+      <div
+        role="presentation"
+        className={`fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10 ${
+          isAsideActive ? '' : 'translate-x-full'
+        }`}
+        onClick={() => setIsAsideActive(false)}
+      />
+      <aside
+        className={`fixed top-0 right-0 w-[300px] h-screen z-20 bg-white dark:bg-[#1C2731] transition-transform duration-200 ease-in-out ${
+          isAsideActive ? '' : 'translate-x-full'
+        }`}
+      >
+        <section className="w-full h-full flex flex-col items-center justify-start">
+          <div
+            role="presentation"
+            className="w-full h-[80px] px-[24px] bg-[#ececec] dark:bg-[#656C73] flex flex-row items-center justify-start border-b-[1px] border-solid b border-[#ececec]"
+            onClick={() => setIsAsideActive(false)}
+          >
+            <BiArrowFromLeft
+              className="cursor-pointer"
+              onClick={() => setIsAsideActive(!isAsideActive)}
+              size="40"
+              color={isDarkThemeActive ? '#cecece' : '#1C2731'}
+            />
+          </div>
+        </section>
+      </aside>
+    </>
   );
 }
