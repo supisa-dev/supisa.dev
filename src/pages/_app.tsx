f065bd766960f2a87abd2ea182576830187b4f7e
SUNG PILL SANG <schmidtiana95@gmail.com>
2023-04-29 15:17:15 +0900

    feat: font display swap 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,38 +9,47 @@ import combineFontVariables from '@/utils/combineFontVariables';
 const scDreamThin = localFont({
   src: '../../public/fonts/SCDream1.woff2',
   variable: '--font-sc-dream-thin',
+  display: 'swap',
 });
 const scDreamExtraLight = localFont({
   src: '../../public/fonts/SCDream2.woff2',
   variable: '--font-sc-dream-extra-light',
+  display: 'swap',
 });
 const scDreamLight = localFont({
   src: '../../public/fonts/SCDream3.woff2',
   variable: '--font-sc-dream-light',
+  display: 'swap',
 });
 const scDreamRegular = localFont({
   src: '../../public/fonts/SCDream4.woff2',
   variable: '--font-sc-dream-regular',
+  display: 'swap',
 });
 const scDreamMedium = localFont({
   src: '../../public/fonts/SCDream5.woff2',
   variable: '--font-sc-dream-medium',
+  display: 'swap',
 });
 const scDreamBold = localFont({
   src: '../../public/fonts/SCDream6.woff2',
   variable: '--font-sc-dream-bold',
+  display: 'swap',
 });
 const scDreamExtraBold = localFont({
   src: '../../public/fonts/SCDream7.woff2',
   variable: '--font-sc-dream-extra-bold',
+  display: 'swap',
 });
 const scDreamHeavy = localFont({
   src: '../../public/fonts/SCDream8.woff2',
   variable: '--font-sc-dream-heavy',
+  display: 'swap',
 });
 const scDreamBlack = localFont({
   src: '../../public/fonts/SCDream9.woff2',
   variable: '--font-sc-dream-black',
+  display: 'swap',
 });
 
 export default function App({Component, pageProps}: AppProps) {
